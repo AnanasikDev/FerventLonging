1dba01ff38d2d747bfda2d456ff3559596426aaa
oleg <oleg.brovchenkov@gmail.com>
2024-07-08 04:28:05 +0200

    Start scene GUI added

---
+++ Assets/UI/Scipts/Menu.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField] private new GameObject light;
+    [SerializeField] private new Camera camera;
+
+    [SerializeField] private Animator animator;
+
+    private void Update()
+    {
+        light.transform.position = Vector3.Lerp(light.transform.position, camera.ScreenToWorldPoint(Input.mousePosition), Time.deltaTime);
+        light.transform.position = light.transform.position.SetZ(9);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Play()
+    {
+        IEnumerator proceed()
+        {
+            animator.SetTrigger("fadeAway");
+            yield return new WaitForSecondsRealtime(2f);
+            SceneManager.LoadScene("Main");
+        }
+
+        StartCoroutine(proceed());
+    }
+}
