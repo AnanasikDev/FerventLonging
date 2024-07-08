858300173cd7394424b6b765ea0eb8d1992c0fc3
oleg <oleg.brovchenkov@gmail.com>
2024-07-08 03:17:14 +0200

    Player sprite & walking animation added; 1.0 built

--- Assets/Player/Control/Scripts/PlayerMovement.cs
+++ Assets/Player/Control/Scripts/PlayerMovement.cs
@@ -8,13 +8,25 @@ public class PlayerMovement : MonoBehaviour
     [ReadOnly] public float currentSpeed;
     [ReadOnly] public Vector2 velocity;
 
+    private Animator animator;
+
     public void Init()
     {
+        animator = GetComponent<Animator>();
         SetSpeed(walkingSpeed);
     }
     public void UpdateMovement()
     {
         velocity = new Vector2(Scripts.Player.playerInput.v_horizonalAxis, Scripts.Player.playerInput.v_verticalAxis) * currentSpeed;
+
+        if (Scripts.Player.playerInput.v_horizonalAxis == 0 && Scripts.Player.playerInput.v_verticalAxis == 0)
+        {
+            animator.SetBool("walking", false);
+        }
+        else
+        {
+            animator.SetBool("walking", true);
+        }
 
         gameObject.transform.position = gameObject.transform.position.ConvertTo2D() + velocity;
     }
