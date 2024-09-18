fbfee78fb5bff47a32ebc449d7ae60e4f1b7b403
oleg <oleg.brovchenkov@gmail.com>
2024-09-18 19:47:12 +0300

    Main Music theme added; MusicPlayer added

---
+++ Assets/UI/MusicPlayer.cs
@@ -0,0 +1,33 @@
+using NaughtyAttributes;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip track;
+
+    [ReadOnly][SerializeField] private float secondsLeft = 0;
+    [SerializeField] private float maxWaitFactor = 3;
+    [SerializeField] private float maxStartWaitFactor = 2;
+
+    private void Start()
+    {
+        secondsLeft = Random.value * maxStartWaitFactor;
+    }
+
+    private void Update()
+    {
+        secondsLeft -= Time.deltaTime;
+        if (secondsLeft <= 0)
+        {
+            secondsLeft = 0;
+            StartMusic();
+        }
+    }
+
+    private void StartMusic()
+    {
+        audioSource.PlayOneShot(track);
+        secondsLeft = track.length * ((Random.value + 1) * maxWaitFactor);
+    }
+}
