20d338f73ef11a1b9f2539b91bc0a398a937e2bc
oleg <oleg.brovchenkov@gmail.com>
2024-10-04 14:14:29 +0200

    Ambient audio added; Death sound added

--- Assets/Player/Control/Scripts/PlayerDeath.cs
+++ Assets/Player/Control/Scripts/PlayerDeath.cs
@@ -10,6 +10,9 @@ public class PlayerDeath : MonoBehaviour
     [SerializeField] private TextMeshProUGUI totals;
     private string totalsFormat = "Fuel collected: {0}\nEnemies killed: {1}\nRooms explored: {2}";
 
+    [SerializeField] private AudioClip[] clips;
+    [SerializeField] private AudioSource source;
+
     private bool isDead = false;
 
     public void Init()
@@ -25,6 +28,9 @@ public class PlayerDeath : MonoBehaviour
 
         totals.text = string.Format(totalsFormat, Fuel.totalFuelCollected, EnemyController.totalKilled, Room._id);
         isDead = true;
+
+        foreach (var clip in clips)
+            source.PlayOneShot(clip);
     }
 
     public void QuitToMenu()
